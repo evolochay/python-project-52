a384200e4cb352aa03600b4d1eb3964ef03b7ce2
volochay <evgenia@volochay.info>
2023-03-07 21:03:05 +0100

    database`s settings were updated

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -77,14 +77,10 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
-DATABASES['default'] = dj_database_url.config()
+DATABASE_URL = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+
+DATABASES = {'default': dj_database_url.config(default=DATABASE_URL)}
 
 
 # DATABASES = {
