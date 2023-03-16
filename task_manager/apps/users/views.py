b93ea53274132e29bd848698a59bfa510a82bd87
volochay <evgenia@volochay.info>
2023-03-16 14:03:00 +0100

    different view for users list was added

--- task_manager/apps/users/views.py
+++ task_manager/apps/users/views.py
@@ -5,5 +5,5 @@ from task_manager.apps.users.models import User
 
 class UserList(ListView):
     def get(self, request, *args, **kwargs):
-        users = User.objects.all
+        users = User.objects.all()
         return render(request, 'users_list.html', context={'users': users})
